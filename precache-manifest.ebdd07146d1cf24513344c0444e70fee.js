0d2932f09a42e0cdd3898cfc73eba27f3da0a01c
Code Sandbox <hello@codesandbox.io>
2022-02-07 17:26:00 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.ebdd07146d1cf24513344c0444e70fee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8afce3dcd298ba58d4984997d417738b",
+    "url": "https://gouravsingh2580.github.io/csb-ig55l/index.html"
+  },
+  {
+    "revision": "051343d312f54ed90043",
+    "url": "https://gouravsingh2580.github.io/csb-ig55l/static/css/main.2142e091.chunk.css"
+  },
+  {
+    "revision": "33866317bb3327cec541",
+    "url": "https://gouravsingh2580.github.io/csb-ig55l/static/js/2.b59b38d8.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "https://gouravsingh2580.github.io/csb-ig55l/static/js/2.b59b38d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "051343d312f54ed90043",
+    "url": "https://gouravsingh2580.github.io/csb-ig55l/static/js/main.6dd02dca.chunk.js"
+  },
+  {
+    "revision": "8b5defdc6bb1532e668c",
+    "url": "https://gouravsingh2580.github.io/csb-ig55l/static/js/runtime-main.95359885.js"
+  }
+]);
